f4ed92f5ee24f18e57017b86ce2f1686bbfb6fb0
Jason Son <jasonson2004@gmail.com>
2025-03-15 11:44:45 -0700

    add(core) server template

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,40 @@
 // server.ts
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+
+export const createMCPServer = async () => {
+  const mcpServer = new McpServer({
+    name: 'hackathon-gcp-server',
+    version: '0.0.1',
+  });
+
+  // start registering prompts
+  // {}
+
+  return mcpServer;
+}
+
+export const runMcpServer = async () => {
+  const server = await createMCPServer();
+  const transport = new StdioServerTransport();
+
+  const handleExit = async () => {
+    try {
+
+    } catch {
+
+    } finally {
+
+    }
+  };
+
+  // cleanup
+  process.on("SIGINT", handleExit);
+  process.on("SIGTERM", handleExit);
+
+  try {
+
+  } catch {
+
+  }
+};
